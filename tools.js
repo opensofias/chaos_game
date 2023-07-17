137832a70bbc2a76a38c891b9ac872c5505162c0
sofia <12581975+opensofias@users.noreply.github.com>
2023-07-17 10:15:30 +0200

    introducing hyperIter

---
+++ tools.js
@@ -0,0 +1,19 @@
+export const hyperIter = (size = [], fun = x => x) => {
+	if (!(size.length) || Math.min (...size) < 1) return []
+
+	const count = (new Array(size.length)).fill(0)
+	const result = []
+	let carry = 0
+	while (!carry) {
+		result.push (fun (count))
+
+		carry ++
+		size.forEach ((target, idx) => {
+			if (carry)
+				if (count[idx] == target -1) count[idx] = 0
+				else {count[idx] ++; carry -- }
+		})
+	}
+
+	return result
+}

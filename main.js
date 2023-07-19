e95f811440951e7595f5700d74167981dd957a60
sofia <12581975+opensofias@users.noreply.github.com>
2023-07-19 22:24:47 +0200

    removing unnessecary punctuation

--- main.js
+++ main.js
@@ -15,25 +15,25 @@ const vecLerp = (v1, v2, factor = .5) =>
 
 // Function to draw a point on the canvas
 function drawPoint(context, point, color) {
-	context.fillStyle = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')';
-	context.fillRect(...point, 1, 1);
+	context.fillStyle = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')'
+	context.fillRect(...point, 1, 1)
 }
 
 const mod = (a, b) => (a % b + b) % b
 
 // Function to initialize the Chaos Game
 function initializeChaosGame(canvas, numPoints) {
-	var context = canvas.getContext('2d');
-	var canvasSize = [canvas.width, canvas.height];
+	var context = canvas.getContext('2d')
+	var canvasSize = [canvas.width, canvas.height]
 
 	// Clear the canvas
 	context.clearRect(0, 0, ...canvasSize);
 
 	// Generate target points forming a circle
-	var targetPoints = [];
-	var center = canvasSize.map (x => x / 2);
-	var radius = Math.min(...canvasSize) / 2;
-	var angleIncrement = (2 * Math.PI) / numPoints;
+	var targetPoints = []
+	var center = canvasSize.map (x => x / 2)
+	var radius = Math.min(...canvasSize) / 2
+	var angleIncrement = (2 * Math.PI) / numPoints
 	const thirdAngle = Math.PI * 2 / 3
 	const rotationFactor = 2
 
@@ -63,8 +63,8 @@ function initializeChaosGame(canvas, numPoints) {
 	// Iterate and draw the chaos game
 	for (var i = 0; i < 1000000; i++) {
 		// Pick a random target point
-		var targetIndex = Math.floor(Math.random() * numPoints);
-		var targetPoint = targetPoints[targetIndex];
+		var targetIndex = Math.floor(Math.random() * numPoints)
+		var targetPoint = targetPoints[targetIndex]
 
 		if (
 			false /* <- replace this with constraint */
@@ -80,7 +80,7 @@ function initializeChaosGame(canvas, numPoints) {
 		};
 
 		// Draw the midpoint
-		drawPoint(context, currentPoint.position, currentPoint.color);
+		drawPoint(context, currentPoint.position, currentPoint.color)
 	}
 }
 
